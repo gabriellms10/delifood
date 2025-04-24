13b9bd6f9313bef86180856253cf7e4d7b39edd7
Gabriel Sa <gabriellms2002@gmail.com>
2025-04-24 07:10:20 -0300

    Criada a Classe RestauranteService e seus Metodos add @Autowired na CozinhaService

--- src/main/java/com/gabriel/Delifood/Service/RestauranteService.java
+++ src/main/java/com/gabriel/Delifood/Service/RestauranteService.java
@@ -1,11 +1,46 @@
 package com.gabriel.Delifood.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.gabriel.Delifood.Entitys.Restaurante;
 import com.gabriel.Delifood.Repository.RestauranteRepository;
 
 @Service
 public class RestauranteService {
-
+	@Autowired
 	private RestauranteRepository repository;
+
+	public void create(Restaurante obj) {
+		repository.save(obj);
+	}
+
+	public void delete(Long Id) {
+		repository.deleteById(Id);
+	}
+
+	public Restaurante GetId(Long Id) {
+		Optional<Restaurante> obj = repository.findById(Id);
+		return obj.get();
+	}
+
+	public List<Restaurante> getAll() {
+		return repository.findAll();
+
+	}
+
+	public Restaurante update(Restaurante obj) {
+		Optional<Restaurante> newObj = repository.findById(obj.getId());
+		updateRestaurante(newObj, obj);
+		return repository.save(newObj.get());
+	}
+
+	private void updateRestaurante(Optional<Restaurante> newObj, Restaurante obj) {
+		newObj.get().setNome(obj.getNome());
+
+	}
+
 }
